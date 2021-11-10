5409d670cec5e15faf9d2a93c46b8f15044e6ff0
prplblck <kariurek@gmail.com>
2021-11-10 22:59:47 +0100

    inital: commit

---
+++ map/map.js
@@ -0,0 +1,13 @@
+// Initialize and add the map
+function initMap() {
+    const uluru = { lat: -25.344, lng: 131.036 };
+    const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 4,
+      center: uluru,
+    });
+    // The marker, positioned at Uluru
+    const marker = new google.maps.Marker({
+      position: uluru,
+      map: map,
+    });
+  }
